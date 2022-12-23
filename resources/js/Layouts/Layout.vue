064e21b716715855f1841842f015dcb8652a0c5a
corean <corean@corean.biz>
2022-12-23 22:05:47 +0900

    - dark mode

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -5,17 +5,22 @@ import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue'
 import { Link } from '@inertiajs/inertia-vue3'
 import UserMenu from '@/Layouts/Partials/UserMenu.vue'
 import NavMenu from '@/Layouts/Partials/NavMenu.vue'
+import { useDark, useToggle } from '@vueuse/core'
 
 const showingNavigationDropdown = ref(false)
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
 </script>
 
 <template>
   <div>
     <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-      <nav class="border-b border-gray-100 bg-white">
+      <nav
+        class="border-b border-gray-100 bg-white dark:border-gray-700 dark:bg-gray-900"
+      >
         <!-- Primary Navigation Menu -->
         <div
-          class="mx-auto max-w-7xl border-b border-gray-200 px-4 sm:px-6 lg:px-8"
+          class="mx-auto max-w-7xl border-b border-gray-200 px-4 dark:border-gray-700 dark:bg-gray-800 sm:px-6 lg:px-8"
         >
           <div class="flex h-14 justify-between">
             <div class="flex">
@@ -29,7 +34,43 @@ const showingNavigationDropdown = ref(false)
               </div>
             </div>
 
-            <UserMenu />
+            <div class="flex items-center">
+              <button
+                @click="toggleDark()"
+                type="button"
+                class="inline-flex items-center rounded-lg p-2.5 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+              >
+                <svg
+                  v-if="isDark"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                  class="h-5 w-5"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+
+                <svg
+                  v-else
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                  class="h-5 w-5"
+                >
+                  <path
+                    d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"
+                  ></path>
+                </svg>
+                <!-- dark mode button -->
+              </button>
+              <UserMenu />
+            </div>
 
             <!-- Hamburger -->
             <div class="-mr-2 flex items-center sm:hidden">
@@ -69,11 +110,7 @@ const showingNavigationDropdown = ref(false)
           </div>
         </div>
         <!-- Navigation Links -->
-        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-          <div class="flex h-14 justify-between">
-            <NavMenu />
-          </div>
-        </div>
+        <NavMenu />
 
         <!-- Responsive Navigation Menu -->
         <div
@@ -107,8 +144,8 @@ const showingNavigationDropdown = ref(false)
 
               <div class="mt-3 space-y-1">
                 <ResponsiveNavLink :href="route('profile.edit')">
-                  Profile</ResponsiveNavLink
-                >
+                  Profile
+                </ResponsiveNavLink>
                 <ResponsiveNavLink
                   :href="route('logout')"
                   method="post"
--- resources/js/Layouts/Partials/NavMenu.vue
+++ resources/js/Layouts/Partials/NavMenu.vue
@@ -2,18 +2,25 @@
 import NavLink from '@/Components/NavLink.vue'
 </script>
 <template>
-  <div class="hidden space-x-8 sm:-my-px sm:flex">
-    <NavLink :href="route('home')" :active="route().current('home')">
-      Home
-    </NavLink>
-    <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
-      Dashboard
-    </NavLink>
-    <NavLink
-      :href="route('admin.memberships.index')"
-      :active="route().current('admin.memberships.index')"
-    >
-      연간회원
-    </NavLink>
+  <div class="mx-auto max-w-7xl bg-white px-4 dark:bg-gray-800 sm:px-6 lg:px-8">
+    <div class="flex h-14 justify-between">
+      <div class="hidden space-x-8 sm:-my-px sm:flex">
+        <NavLink :href="route('home')" :active="route().current('home')">
+          Home
+        </NavLink>
+        <NavLink
+          :href="route('dashboard')"
+          :active="route().current('dashboard')"
+        >
+          Dashboard
+        </NavLink>
+        <NavLink
+          :href="route('admin.memberships.index')"
+          :active="route().current('admin.memberships.index')"
+        >
+          연간회원
+        </NavLink>
+      </div>
+    </div>
   </div>
 </template>
