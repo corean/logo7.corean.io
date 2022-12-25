e0fea39c4900b30be89e90cef2ba95797d2a8929
corean <corean@corean.biz>
2022-12-25 22:02:44 +0900

    - Welcome 페이지 : tailwindcss prefix 추가

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -13,39 +13,42 @@ defineProps({
   <Head title="Welcome" />
 
   <div
-    class="items-top relative flex min-h-screen justify-center bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0"
+    class="_items-top _relative _flex _min-h-screen _justify-center _bg-gray-100 dark:_bg-gray-900 sm:_items-center sm:_pt-0"
   >
-    <div v-if="canLogin" class="fixed top-0 right-0 hidden px-6 py-4 sm:block">
+    <div
+      v-if="canLogin"
+      class="_fixed _top-0 _right-0 _hidden _px-6 _py-4 sm:_block"
+    >
       <Link
         v-if="$page.props.auth.user"
         :href="route('home')"
-        class="text-sm text-gray-700 dark:text-gray-500"
+        class="_text-sm _text-gray-700 dark:_text-gray-500"
         >home
       </Link>
 
       <template v-else>
         <Link
           :href="route('login')"
-          class="text-sm text-gray-700 dark:text-gray-500"
+          class="_text-sm _text-gray-700 dark:_text-gray-500"
           >{{ __('Log in') }}
         </Link>
 
         <Link
           v-if="canRegister"
           :href="route('register')"
-          class="ml-4 text-sm text-gray-700 dark:text-gray-500"
+          class="_ml-4 _text-sm _text-gray-700 dark:_text-gray-500"
           >{{ __('Register') }}
         </Link>
       </template>
     </div>
 
-    <div class="mx-auto max-w-6xl sm:px-6 lg:px-8">
-      <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
+    <div class="_mx-auto _max-w-6xl sm:_px-6 lg:_px-8">
+      <div class="_flex _justify-center _pt-8 sm:_justify-start sm:_pt-0">
         <svg
           viewBox="0 0 119 71"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
-          class="ml-2 h-16 w-auto text-gray-700 sm:h-20"
+          class="_ml-2 _h-16 _w-auto _text-gray-700 sm:_h-20"
         >
           <g>
             <polygon
@@ -233,11 +236,11 @@ defineProps({
       </div>
 
       <div
-        class="mt-8 overflow-hidden bg-white shadow dark:bg-gray-800 sm:rounded-lg"
+        class="_mt-8 _overflow-hidden _bg-white _shadow dark:_bg-gray-800 sm:_rounded-lg"
       >
-        <div class="grid grid-cols-1 md:grid-cols-2">
-          <div class="p-6">
-            <div class="flex items-center">
+        <div class="_grid _grid-cols-1 md:_grid-cols-2">
+          <div class="_p-6">
+            <div class="_flex _items-center">
               <svg
                 fill="none"
                 stroke="currentColor"
@@ -245,23 +248,23 @@ defineProps({
                 stroke-linejoin="round"
                 stroke-width="2"
                 viewBox="0 0 24 24"
-                class="h-8 w-8 text-gray-500"
+                class="_h-8 _w-8 _text-gray-500"
               >
                 <path
                   d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
                 ></path>
               </svg>
-              <div class="ml-4 text-lg font-semibold leading-7">
+              <div class="_ml-4 _text-lg _font-semibold _leading-7">
                 <a
                   href="http://logo.corean.biz"
-                  class="text-gray-900 underline dark:text-white"
+                  class="_text-gray-900 _underline dark:_text-white"
                   >logo.corean.biz</a
                 >
               </div>
             </div>
 
-            <div class="ml-12">
-              <div class="mt-2 text-sm text-gray-600 dark:text-gray-400">
+            <div class="_ml-12">
+              <div class="_mt-2 _text-sm _text-gray-600 dark:_text-gray-400">
                 현재 리뉴얼을 위해 열심히 작업 중입니다.
               </div>
             </div>
@@ -269,9 +272,11 @@ defineProps({
         </div>
       </div>
 
-      <div class="mt-4 flex justify-center sm:items-center sm:justify-between">
+      <div
+        class="_mt-4 _flex _justify-center sm:_items-center sm:_justify-between"
+      >
         <div
-          class="ml-4 text-center text-sm text-gray-500 sm:ml-0 sm:text-right"
+          class="_ml-4 _text-center _text-sm _text-gray-500 sm:_ml-0 sm:_text-right"
         >
           Laravel v{{ laravelVersion }} (PHP v{{ phpVersion }})
         </div>
