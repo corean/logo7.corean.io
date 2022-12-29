f6b12fc226272b7a57c94dbb7161f55c3f6ae2a5
corean <corean@corean.biz>
2022-12-29 18:53:02 +0900

    - 연간회원 목록 : 검색폼 다듬기

--- resources/js/Pages/Admin/Memberships/Index.vue
+++ resources/js/Pages/Admin/Memberships/Index.vue
@@ -36,14 +36,38 @@ const form = useForm({
                 <form
                   @submit.prevent="form.get(route('admin.memberships.index'))"
                 >
-                  Search:
-                  <div class="ms-2 d-inline-block">
-                    <input
-                      type="text"
-                      v-model="form.search"
-                      class="form-control form-control-sm"
-                      aria-label="Search invoice"
-                    />
+                  <div class="d-none d-md-block">
+                    <div class="input-icon">
+                      <span class="input-icon-addon">
+                        <!-- Download SVG icon from https://tabler-icons.io/i/search -->
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          class="icon icon-tabler icon-tabler-search"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          stroke-width="1.5"
+                          stroke="currentColor"
+                          fill="none"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        >
+                          <path
+                            stroke="none"
+                            d="M0 0h24v24H0z"
+                            fill="none"
+                          ></path>
+                          <circle cx="10" cy="10" r="7"></circle>
+                          <line x1="21" y1="21" x2="15" y2="15"></line>
+                        </svg>
+                      </span>
+                      <input
+                        type="text"
+                        v-model="form.search"
+                        class="form-control"
+                        placeholder="Search…"
+                      />
+                    </div>
                   </div>
                 </form>
               </div>
