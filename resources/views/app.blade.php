d5129ba3b97917cb698de6d27370883797b33797
corean <corean@corean.biz>
2023-01-15 20:31:11 +0900

    - 파비콘 favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,11 @@
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+    <!-- Icons -->
+    <link rel="shortcut icon" href="{{ asset('media/favicons/favicon.png') }}">
+    <link rel="icon" sizes="192x192" type="image/png" href="{{ asset('media/favicons/favicon-192x192.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('media/favicons/apple-touch-icon-180x180.png') }}">
+
     <!-- Fonts -->
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     <link href='//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css' rel='stylesheet' type='text/css'>
