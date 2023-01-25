a04758d3834669e6d83cd1aa349344f66952c791
corean <corean@corean.biz>
2023-01-25 18:26:37 +0900

    - envoy first init

---
+++ Envoy.blade.php
@@ -0,0 +1,46 @@
+@servers(['web' => 'logo2@logo.corean.io'])
+
+@task('list', ['on' => 'web'])
+ls -l
+@endtask
+
+@setup
+    $repository = 'git@github.com:corean/logo7.corean.io.git';
+    $releases_dir = '/home/logo2/releases';
+    $app_dir = '/home/logo2/www';
+    $release = date('YmdHis');
+    $new_release_dir = $releases_dir .'/'. $release;
+@endsetup
+
+@story('deploy')
+    clone_repository
+    run_composer
+    update_symlinks
+@endstory
+
+@task('clone_repository')
+    echo 'Cloning repository'
+    [ -d {{ $releases_dir }} ] || mkdir {{ $releases_dir }}
+    git clone --depth 1 {{ $repository }} {{ $new_release_dir }}
+    cd {{ $new_release_dir }}
+    git reset --hard {{ $commit }}
+@endtask
+
+@task('run_composer')
+    echo "Starting deployment ({{ $release }})"
+    cd {{ $new_release_dir }}
+    composer install --prefer-dist --no-scripts -q -o
+@endtask
+
+@task('update_symlinks')
+    echo "Linking storage directory"
+    rm -rf {{ $new_release_dir }}/storage
+    ln -nfs {{ $app_dir }}/storage {{ $new_release_dir }}/storage
+
+    echo 'Linking .env file'
+    ln -nfs {{ $app_dir }}/.env {{ $new_release_dir }}/.env
+
+    echo 'Linking current release'
+    ln -nfs {{ $new_release_dir }} {{ $app_dir }}/current
+@endtask
+
