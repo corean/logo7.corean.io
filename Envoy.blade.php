c236f6c5656aa51b8f2953297d5c6c88bdcb32ca
corean <corean@corean.biz>
2023-01-25 18:35:13 +0900

    - envoy 경로 변경

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -6,8 +6,8 @@ ls -l
 
 @setup
     $repository = 'git@github.com:corean/logo7.corean.io.git';
-    $releases_dir = '/home/logo2/releases';
-    $app_dir = '/home/logo2/www';
+    $releases_dir = '/home3/logo2/releases';
+    $app_dir = '/home3/logo2/';
     $release = date('YmdHis');
     $new_release_dir = $releases_dir .'/'. $release;
 @endsetup
