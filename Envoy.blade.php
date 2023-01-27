7e6c7c742345902ac647e612e2a0d2e0bbedf396
corean <corean@corean.biz>
2023-01-27 12:36:27 +0900

    - js 파일을 못찾는 오류가 있어서 Envoy 원복

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -56,12 +56,10 @@ ls -l
 @task('optimize')
     echo "optimize laravel"
     cd {{ $new_release_dir }}
-    php artisan optimize:clear
     php artisan config:cache
     php artisan route:cache
     php artisan view:cache
     php artisan event:cache
-    php artisan optimize
 @endtask
 
 
